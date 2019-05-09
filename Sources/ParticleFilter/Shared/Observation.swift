a3083aed6e20a49238775c8e9d9f5c17f6ec5c95
Vincent Esche <regexident@gmail.com>
2019-05-09 16:33:24 +0200

    Added implementation of stochastic 3D sphere sampling

--- Sources/ParticleFilter/Shared/Observation.swift
+++ Sources/ParticleFilter/Shared/Observation.swift
@@ -7,3 +7,53 @@ public struct Observation: Equatable {
     public var xyz: Vector
     public var measurement: Scalar
 }
+
+extension Observation {
+    /// Returns random sample on sphere with
+    /// - radius: `observation.measurement`
+    /// - center: `observation.xyz`
+    /// and a guassian distribution on radius
+    /// with std-deviation of `stdDeviation`.
+    public func sample(stdDeviation: Double) -> Vector {
+        let uniform = DefaultUniformRandom()
+        let gaussian = DefaultGaussianRandom()
+        return self.sample(
+            stdDeviation: stdDeviation,
+            uniformRandom: uniform,
+            gaussianRandom: gaussian
+        )
+    }
+    
+    /// Returns random sample on sphere with
+    /// - radius: `observation.measurement`
+    /// - center: `observation.xyz`
+    /// and a guassian distribution on radius
+    /// with std-deviation of `stdDeviation`.
+    internal func sample<U, G>(
+        stdDeviation: Double,
+        uniformRandom: U,
+        gaussianRandom: G
+    ) -> Vector
+        where U: UniformRandom, U.Value == Double, G: GaussianRandom, G.Value == Double
+    {
+        var uniformRandom = uniformRandom
+        var gaussianRandom = gaussianRandom
+        
+        let xyz = self.xyz
+        let radius = gaussianRandom.random(
+            mean: self.measurement,
+            stdDeviation: stdDeviation
+        )
+        
+        let v = uniformRandom.random()
+        
+        let theta = uniformRandom.random(in: 0.0..<(2.0 * .pi))
+        let phi = acos((2.0 * v) - 1.0)
+
+        let x = xyz.x + (radius * sin(phi) * cos(theta))
+        let y = xyz.y + (radius * sin(phi) * sin(theta))
+        let z = xyz.z + (radius * cos(phi))
+        
+        return [x, y, z]
+    }
+}
